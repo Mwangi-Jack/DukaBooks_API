00a080c0ef3b6ec0bf5abce8fefc16109fe1542b
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-06-10 17:33:58 +0300

    added path

--- api/server.js
+++ api/server.js
@@ -19,9 +19,10 @@
 
 
 const jsonServer = require('json-server');
+const path = require('path')
 const cors = require('cors');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router(path.join(__dirname, '../db.json'))
 const middlewares = jsonServer.defaults();
 
 
